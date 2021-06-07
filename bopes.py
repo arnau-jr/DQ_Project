Tried to fix normalization, failed

bopes.py
@@ -103,7 +103,7 @@ for i in range(0,NR):
 
     for j in range(0,N_states):
         eigenvalues[i,j] = W[j]
-        eigenstates[i,:,j] = V[:,j]/np.sqrt(np.sum(V[:,j]**2)*dr)
+        eigenstates[i,:,j] = V[:,j]/np.sqrt(np.sum(np.abs(V[:,j])**2)*dr)
 
 
 with open("eigenvalues.npy","wb") as f:

parameters.py
@@ -6,10 +6,10 @@ R_f = 5.
 R_l = 4.
 R_r = 3.1
 
-dr = 0.25
+dr = 1.
 dR = 0.25
 
-r_array = np.arange(-120.,120.,dr)
+r_array = np.arange(-60.,60.,dr)
 R_array = np.arange(-9.,9.,dR)
 
 Nr = np.size(r_array)

wave_prop.py
@@ -31,11 +31,13 @@ wave_p = wave_packet(x=R_array)
 wave_p_vec = matlib.repmat(wave_p,1,Nr).T
 phi0 = ex1.flatten()*wave_p_vec
 wave = phi0.sum(axis=0)
+
 #### IT IS NORMED FOR NOW SINCE THE EIGENSTATES ARE NOT
+## A: it seems that 
 wave = wave/(np.sqrt(np.sum(np.abs(wave)**2)*dr*dR))
+print(np.sum(np.abs(wave)**2)*dr*dR)
 
 
-dt = 1e-4
 #endtime = 30/(2.418884e-2)
 endtime = 0.01
 
@@ -68,8 +70,8 @@ def simulate(psi,hamiltonian,dt,endtime,snaps):
     
     return psi_evolved,nucleus_evolved
 
-full_hamiltonian_mat = fh.build_hamiltonian()
-psi_evolved,nucleus_evolved = simulate(psi=psi0,hamiltonian=full_hamiltonian_mat,dt=1e-7,endtime=1e-5,snaps=10)
+# full_hamiltonian_mat = fh.build_hamiltonian()
+# psi_evolved,nucleus_evolved = simulate(psi=psi0,hamiltonian=full_hamiltonian_mat,dt=1e-7,endtime=1e-5,snaps=10)
 
 with open("psi_evolved.npy","wb") as f:
     np.save(f,psi_evolved)