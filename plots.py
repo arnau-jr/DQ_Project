9ef65041dca8e84fe7635202c728b93e57d44c96
SancarSemihCan <sancar.semihcan@gmail.com>
2021-06-10 14:22:19 +0200

    minor changes with the plot file

--- plots.py
+++ plots.py
@@ -19,7 +19,7 @@ fig = plt.figure(figsize=(figsize_x * 2, figsize_y))
 ax_3d = fig.add_subplot(121, projection='3d')
 
 cont_3d = ax_3d.plot_surface(
-    XX, YY, eig0,                  # x, y, and z values as 2D-arrays
+    XX, YY, eig0,                # x, y, and z values as 2D-arrays
    cstride=1,                    # Downsampling in one direction
    rstride=1,                    # Downsampling in other direction
    linewidth=10,                 
@@ -43,12 +43,10 @@ plt.colorbar(cont_2d)  # Plots the colorbar for 2d contour diagram
 
 # Loop over both axes to label them and adjust the tick sizes
 for a in fig.axes[:-1]:
-    a.set_ylabel('y', size=10)
-    a.set_xlabel('x', size=10)
+    a.set_ylabel('R', size=10)
+    a.set_xlabel('r', size=10)
     a.tick_params(labelsize=5)
 
-#plt.savefig(r"pics\groundstate.png")
-#plt.savefig(r"pics\first_excited.png")
+#plt.savefig("pics/groundstate.png")
+#plt.savefig("pics/first_excited.png")
 plt.savefig("pics/second_excited.png")
-
-
