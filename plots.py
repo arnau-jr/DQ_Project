Made 3D plots prettier (and with correct units)

plots.py
@@ -9,7 +9,7 @@ eigenstates = np.load("eigenstates.npy")
 # Meshgrid conviently produces 2D-Grids
 XX, YY = np.meshgrid(r_array, R_array)
 
-eig0 = np.abs(eigenstates[:,:,2])
+eig0 = np.abs(eigenstates[:,:,0])
 
 # Create a new figure
 figsize_x, figsize_y = mpl.rcParams["figure.figsize"]
@@ -32,29 +32,29 @@ cont_3d = ax_3d.plot_surface(
 # Add a second, regular subplot for the 2D contour plot
 ax_2d = fig.add_subplot(122)
 cont_2d = ax_2d.contourf(
-    eig0, 
+    XX,YY,eig0, 
     50,              # Sampling rate in each direction
     vmin=0,          # Bottom of colorbar, same as above
     vmax=1,          # Top of colorbar, same as above
     cmap=mpl.cm.jet  # Colormap
     )
 
-plt.colorbar(cont_2d)  # Plots the colorbar for 2d contour diagram
+plt.colorbar(cont_2d,label=r"$|\Psi(r,R)|$")  # Plots the colorbar for 2d contour diagram
 
 # Loop over both axes to label them and adjust the tick sizes
 for a in fig.axes[:-1]:
-    a.set_ylabel(r'R($a_0$)', size=10)
-    a.set_xlabel(r'r($a_0$)', size=10)
+    a.set_ylabel(r'$R(\rm a_0)$', size=10)
+    a.set_xlabel(r'$r(\rm a_0)$', size=10)
     a.tick_params(labelsize=5)
 
-ax_3d.set_zlabel('energy [a.u.]', size = 10)
+ax_3d.set_zlabel(r"$|\Psi(r,R)|$", size = 10)
 
-# fig.suptitle("Ground state")
+fig.suptitle("Ground state")
 # fig.suptitle("First excited state")
-fig.suptitle("Second excited state")
+# fig.suptitle("Second excited state")
 
 
 
-# plt.savefig("pics/groundstate.png")
+plt.savefig("pics/groundstate.png")
 # plt.savefig("pics/first_excited.png")
-plt.savefig("pics/second_excited.png")
+# plt.savefig("pics/second_excited.png")